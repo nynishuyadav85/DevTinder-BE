a4429d5116f50f703424f405fd542d4060ba79c4
Nishant Yadav <nynishuyadav85@gmail.com>
2025-02-06 10:33:13 +0530

    added userSchema and updated a user in Database

--- src/app.js
+++ src/app.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const app = express();
-const connectDB = require('./config/database')
+const connectDB = require('./config/database');
+const User = require('./models/user');
 
-app.get('/user', (req, res) => {
-    res.send("Hello")
+app.post('/signup', async (req, res) => {
+    const user = new User({
+        firstName: "Nishant",
+        lastName: "yadav",
+        email: "ny@y.com",
+        password: "ny123",
+        age: 25
+    })
+
+    try {
+        await user.save();
+        res.send("Added Data in DB")
+    } catch (error) {
+        res.status(400).send("User not added: " + error.message)
+    }
 })
 
 connectDB().then(() => {
