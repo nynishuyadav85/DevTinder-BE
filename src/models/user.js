02df08a9cb2cd767f76b6eb3026e670da4ac3a5e
Nishant Yadav <nynishuyadav85@gmail.com>
2025-02-08 05:09:20 +0530

    done Schema validation

--- src/models/user.js
+++ src/models/user.js
@@ -3,20 +3,49 @@ const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
     firstName: {
-        type: String
+        type: String,
+        required: true
     },
     lastName: {
         type: String
     },
     email: {
-        type: String
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true
     },
     password: {
-        type: String
+        type: String,
+        required: true,
+        minLength: 8
     },
     age: {
-        type: Number
+        type: Number,
+        min: 18
+    },
+    gender: {
+        type: String,
+        validate(value) {
+            if (!["male", "female", "others"].includes(value)) {
+                throw new Error("Gender Data is not valid")
+            }
+        }
+    },
+    photoUrl: {
+        type: String,
+        default: "photurl.png"
+    },
+    about: {
+        type: String,
+        default: "This is default about"
+    },
+    skills: {
+        type: [String],
     }
+}, {
+    timeStamps: true
 });
 
 
