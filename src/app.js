8050c771882550ff632d6407c7ffa412502280ca
Nishant Yadav <nynishuyadav85@gmail.com>
2025-02-13 09:16:32 +0530

    added logic to expire token and cookies

--- src/app.js
+++ src/app.js
@@ -37,8 +37,8 @@ app.post('/login', async (req, res) => {
         }
         const isPasswordValid = await bcrypt.compare(password, user.password)
         if (isPasswordValid) {
-            const token = await jwt.sign({ _id: user._id }, '123ABC')
-            res.cookie('token', token)
+            const token = await jwt.sign({ _id: user._id }, '123ABC', { expiresIn: "7d" })
+            res.cookie('token', token, { expires: new Date(Date.now() + 8 * 3600000) })
             res.send("Logged In")
         } else {
             throw new Error("Password is not valid")
