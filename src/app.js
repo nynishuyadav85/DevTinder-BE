5a261cd5f4b249e356fe0d4a523bc479b3970edc
Nishant Yadav <nynishuyadav85@gmail.com>
2025-04-15 00:19:50 +0530

    added cors package and update a code with cors middlewatre

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const connectDB = require('./config/database');
 const cookieparser = require('cookie-parser')
+const cors = require('cors')
+
+app.use(cors())
 app.use(express.json())
 app.use(cookieparser())
 
