f61158303da518ed19a18dff511124d2e488a9e3
Nishant Yadav <nynishuyadav85@gmail.com>
2025-04-14 12:27:07 +0530

    created a endpoint to see all of the connections user is having

--- src/routes/user.js
+++ src/routes/user.js
@@ -19,6 +19,30 @@ userRouter.get('/users/requests/received', userAuth, async (req, res) => {
     }
 })
 
+userRouter.get('/users/connections', userAuth, async (req, res) => {
+    try {
+        const loggedInUser = req.user
+        const connectionRequests = await ConnectionRequestModel.find({
+            $or: [
+                { toUserId: loggedInUser._id, status: "accepted" },
+                { fromUserId: loggedInUser._id, status: "accepted" },
+            ]
+        }).populate("fromUserId", ["firstName", "lastName", "age", "gender", "photoUrl", "about", "skills"])
+            .populate("toUserId", ["firstName", "lastName", "age", "gender", "photoUrl", "about", "skills"])
+        const data = connectionRequests.map((row) => {
+            if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
+                return row.toUserId
+            }
+            return row.fromUserId
+        })
+
+        res.json({ message: "Data fetched successfully", data })
+    } catch (error) {
+        res.status(400).send("ERROR: " + error.message)
+    }
+
+})
+
 module.exports = {
     userRouter
 }
