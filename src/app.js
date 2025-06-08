d08b5f49e973df6bff5993347ada17f4df16edcf
Nishant Yadav <nynishuyadav85@gmail.com>
2025-06-08 11:09:56 +0530

    fix: implement socket.io integration for real-time communication

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const connectDB = require('./config/database');
 const cookieparser = require('cookie-parser')
 const cors = require('cors')
 require('dotenv').config()
+const http = require('http')
 
 app.use(cors({
     origin: "http://localhost:5173",
@@ -16,15 +17,19 @@ const authRouter = require('./routes/auth')
 const profileRouter = require('./routes/profile')
 const requestRouter = require('./routes/request');
 const { userRouter } = require('./routes/user');
+const initlizeSocket = require('./utils/socket');
 
 app.use('/', authRouter)
 app.use('/', profileRouter)
 app.use('/', requestRouter)
 app.use('/', userRouter)
 
+const server = http.createServer(app)
+initlizeSocket(server)
+
 connectDB().then(() => {
     console.log("DB connected")
-    app.listen(process.env.PORT)
+    server.listen(process.env.PORT)
 }).catch((err) => {
     console.error("DB not connected")
 })
